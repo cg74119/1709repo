46a712f22aa8dcca3619832bfb80232be6c0a346
cgroot <cg123@126.com>
2018-02-26 05:22:13 -0800

    33 commit

--- login.php
+++ login.php
@@ -1,5 +1,7 @@
 <?php
-  echo "this is the first function";
+  echo "this is the 11 function";
 
-  echo "this is the first function";
+  echo "this is the 22 function";
+
+  echo "this is the 33 function";
 ?>
